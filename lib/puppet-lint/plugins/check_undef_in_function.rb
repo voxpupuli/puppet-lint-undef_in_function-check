82c5d2fe3c096461bc95851ab608d1fda316b357
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-02-02 16:18:33 +0100

    Use cache for function_indexes

--- lib/puppet-lint/plugins/check_undef_in_function.rb
+++ lib/puppet-lint/plugins/check_undef_in_function.rb
@@ -1,35 +1,37 @@
 PuppetLint.new_check(:undef_in_function) do
   def function_indexes
-    functions = []
-    tokens.each_with_index do |token, token_idx|
-      if token.type == :NAME && \
-        (token_idx == 0 || (token_idx == 1 && tokens[0].type == :WHITESPACE) || token.prev_token.type == :NEWLINE || token.prev_token.type == :INDENT || \
-          # function in a function
-          (token.prev_code_token && token.prev_code_token.type == :LPAREN))
+    @function_indexes ||= Proc.new do
+      functions = []
+      tokens.each_with_index do |token, token_idx|
+        if token.type == :NAME && \
+          (token_idx == 0 || (token_idx == 1 && tokens[0].type == :WHITESPACE) || token.prev_token.type == :NEWLINE || token.prev_token.type == :INDENT || \
+            # function in a function
+            (token.prev_code_token && token.prev_code_token.type == :LPAREN))
 
-          level = 0
-          real_idx = 0
-          in_paren = false
-          tokens[token_idx+1..-1].each_with_index do |cur_token, cur_token_idx|
-            break if level == 0 && in_paren
-            break if level == 0 && cur_token.type == :NEWLINE
+            level = 0
+            real_idx = 0
+            in_paren = false
+            tokens[token_idx+1..-1].each_with_index do |cur_token, cur_token_idx|
+              break if level == 0 && in_paren
+              break if level == 0 && cur_token.type == :NEWLINE
 
-            if cur_token.type == :LPAREN
-              level += 1
-              in_paren = true
+              if cur_token.type == :LPAREN
+                level += 1
+                in_paren = true
+              end
+              level -= 1 if cur_token.type == :RPAREN
+              real_idx = token_idx + 1 + cur_token_idx
             end
-            level -= 1 if cur_token.type == :RPAREN
-            real_idx = token_idx + 1 + cur_token_idx
-          end
 
-          functions << {
-            :start  => token_idx,
-            :end    => real_idx,
-            :tokens => tokens[token_idx..real_idx],
-          }         
+            functions << {
+              :start  => token_idx,
+              :end    => real_idx,
+              :tokens => tokens[token_idx..real_idx],
+            }         
+        end
       end
-    end
-    functions
+      functions
+    end.call
   end
 
   def check
